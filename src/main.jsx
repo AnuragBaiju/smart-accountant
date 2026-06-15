2af6d7f80321046e5e4cd80447129f71354652ee
AnuragBaiju <anuragbaiju03@gmail.com>
2026-06-15 17:01:13 +0100

    feat: wire up cognito env variables safely

--- src/main.jsx
+++ src/main.jsx
@@ -3,24 +3,27 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// 1. Import Amplify
 import { Amplify } from 'aws-amplify';
 
+// 2. Configure cross-region backend
 Amplify.configure({
   Auth: {
     Cognito: {
       userPoolId: import.meta.env.VITE_COGNITO_USER_POOL_ID,
       userPoolClientId: import.meta.env.VITE_COGNITO_CLIENT_ID,
-      region: 'eu-west-1', // Your Cognito is in Ireland
+      region: 'eu-west-1', // Auth database is in Ireland
     }
   },
   Storage: {
     S3: {
       bucket: import.meta.env.VITE_STORAGE_BUCKET,
-      region: 'us-east-1' // Your S3 Bucket is in Virginia
+      region: 'us-east-1' // S3 Bucket is in N. Virginia
     }
   }
 });
 
+// 3. Render the app
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
