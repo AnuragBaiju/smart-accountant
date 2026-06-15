0b832f9bc6b453025176f3b26cfe2050e2c2e588
AnuragBaiju <anuragbaiju03@gmail.com>
2026-06-15 17:04:01 +0100

    fix: resolve NoBucket error and map cross-region infrastructure

--- src/main.jsx
+++ src/main.jsx
@@ -2,28 +2,24 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
-// 1. Import Amplify
 import { Amplify } from 'aws-amplify';
 
-// 2. Configure cross-region backend
 Amplify.configure({
   Auth: {
     Cognito: {
-      userPoolId: import.meta.env.VITE_COGNITO_USER_POOL_ID,
-      userPoolClientId: import.meta.env.VITE_COGNITO_CLIENT_ID,
-      region: 'eu-west-1', // Auth database is in Ireland
+      userPoolId: 'eu-west-1_EVhy3v8FD',
+      userPoolClientId: 'ku9og12s7nl351s57592npaue',
+      region: 'eu-west-1', 
     }
   },
   Storage: {
     S3: {
-      bucket: import.meta.env.VITE_STORAGE_BUCKET,
-      region: 'us-east-1' // S3 Bucket is in N. Virginia
+      bucket: 'smart-accountant-upload-049145893383',
+      region: 'us-east-1' 
     }
   }
 });
 
-// 3. Render the app
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
